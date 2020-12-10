6a18916e1faa79c1c3dc33089ba8fd75e2953c9b
Armando Perea <terry900407@gmail.com>
2020-12-09 21:36:22 -0600

    Ready Find number ten operation

--- src/main/java/com/java/exercises/operations/MathemathicOperations.java
+++ src/main/java/com/java/exercises/operations/MathemathicOperations.java
@@ -9,13 +9,34 @@ public class MathemathicOperations {
 		return result;
 	}
 	
+	public double getMultiply(double numA, double numB) {
+		double result;
+		result = numA*numB; // + - * / %
+		return result;
+	}
+	
+	public String findNumberTen(double number) {
+		String message;
+		// Si el numero recibido es 10 entonces regresa mensaje de ENCONTRADO
+		// Si el numero recibino NO es 10 entonces regresa mensaje de NO ENCONTRADO
+		
+		// == < > <= >= !
+		if(number==10) {
+			message = "ENCONTRADO";
+		}else {
+			message= "NO ENCONTRADO";
+		}
+		
+		return message;
+	}
+	
 	// char byte int float double long
 	public static void main(String [] args ) {
 		MathemathicOperations operation = new MathemathicOperations();
 		double result;
-		result = operation.getResidue(10, 3);
-		
-		System.out.println("Result: "+result);
+		String message;
+		message = operation.findNumberTen(1);
+		System.out.println("Mensaje: "+message);
 		
 	}
 	
